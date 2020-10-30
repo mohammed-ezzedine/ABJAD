c67d113b98342c5c67e46af91c1259a44fc46da0
Mohammed Ezzedine <mezdn@outlook.com>
2020-10-30 22:12:52 +0200

    Add reader

---
+++ ABJAD.Reader/Reader.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace ABJAD.Reader
+{
+    public class Reader
+    {
+        public static string Read(string fileName)
+        {
+            if (!IsAbjadFile(fileName))
+            {
+                // TODO: throw new EZInvalidFileException();
+            }
+
+            if (!File.Exists(fileName))
+            {
+                // TODO: throw new EZFileNotFoundException(fileName);
+            }
+
+            var code = File.ReadAllText(fileName);
+            return code;
+        }
+
+        private static bool IsAbjadFile(string fileName)
+        {
+            if (fileName.Split('.').Length < 2)
+            {
+                return false;
+            }
+
+            var extension = fileName.Split('.').Last().ToLower();
+            if (extension == "abjad")
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
